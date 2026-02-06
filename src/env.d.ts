55be27ee741060c3813abba8ffbf0c0cdae06b41
Matloob11 <matloobulhassnain11@gmail.com>
2026-02-06 21:28:24 +0500

    Contact: Configured EmailJS with user credentials and updated environment types

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_TEMPLATE_ID: string;
   readonly VITE_APP_EMAILJS_RECIEVER: string;
   readonly VITE_APP_EMAILJS_KEY: string;
+  readonly VITE_GITHUB_TOKEN: string;
   // more env variables...
 }
 
